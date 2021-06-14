04111b664ec2507833fb953ecf73c1a2400f6fe2
sachin099 <sachin.choudhary58@gmail.com>
2021-06-14 14:42:27 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+// Java program to demonstrate the
+// working of PriorityQueue
+import java.util.*;
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  name = 'Angular';
+class PriorityQueueDemo {
+
+	public static void main(String args[])
+	{
+		PriorityQueue<String> pq = new PriorityQueue<>();
+		 
+        pq.add("Adding");
+        pq.add("to");
+        pq.add("PQueue");
+
+		System.out.println("Initial PriorityQueue " + pq);
+		 
+		System.out.println(pq.peek());
+      pq.remove("to");
+
+      System.out.println("After Remove - " + pq);
+
+      System.out.println("Poll Method - " + pq.poll());
+
+      System.out.println("Final PriorityQueue - " + pq);
+	}
 }
